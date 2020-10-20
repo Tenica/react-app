921c863858dac9cf129275541a288d7c3a5b06bd
Chika Onyema <chika.onyema01@gmail.com git config --gobal user.name Chika>
2020-10-20 03:47:29 +0100

    changed slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -9,7 +9,7 @@ return     <section className="home-slider owl-carousel">
   <div className="container">
     <div className="row slider-text align-items-center justify-content-center">
       <div className="col-md-10 text-center col-sm-12 element-animate">
-        <h1>Free Template by Free-Template.co</h1>
+        <h1>Welcome to Trailblazers Insurance Brokers</h1>
         <p className="mb-5 sub-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi unde impedit, necessitatibus, soluta sit quam minima expedita atque corrupti reiciendis.</p>
         <p><Link to ="#" className="btn btn-white btn-outline-white px-3 py-3">Get Started</Link> <Link to ="#" className="btn btn-link btn-white">Download</Link></p>
       </div>
